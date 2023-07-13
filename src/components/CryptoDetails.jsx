720b9107bea282d5dcd8ba4500b8c48e8dae3c63
Branden Ng <brandendanielng@gmail.com>
2023-07-13 12:38:40 +1000

    progress

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -97,92 +97,55 @@ const CryptoDetails = () => {
   ];
 
   return (
-    <Col className="coin-details-container">
-      <Col className="coin-heading-container">
-        <Title level={2} className="coin-name">
-          {cryptoDetails.name} ({cryptoDetails.symbol}) Price
-        </Title>
-        <p>
-          {cryptoDetails.name} live price in US dollars. View value statistics,
-          market cap and supply.
-        </p>
-      </Col>
-      <Select
-        defaultValue="7d"
-        className="select-timeperiod"
-        placeholder="Select Time Period"
-        onChange={(value) => setTimePeriod(value)}
-      >
-        {time.map((date) => (
-          <Option key={date}>{date}</Option>
-        ))}
-      </Select>
-      {/* <LineChart
-        coinHistory={coinHistory}
-        currentPrice={millify(cryptoDetails.price)}
-        coinName={cryptoDetails.name}
-      /> */}
-      <Col className="stats-container">
-        <Col className="coin-value-statistics">
-          <Col className="coin-value-statistics-heading">
-            <Title level={3} className="coin-details-heading">
-              {cryptoDetails.name} View Statistics
-            </Title>
-            <p>An overview showing the stats of {cryptoDetails.name}</p>
-          </Col>
-          {stats.map(({ icon, title, value }) => (
-            <Col className="coin-stats">
-              <Col className="coin-stats-name">
-                <Text>{icon}</Text>
-                <Text>{title}</Text>
-              </Col>
-              <Text className="stats">{value}</Text>
-            </Col>
-          ))}
-        </Col>
+    <div className="main__coinDetails">
+      <div className="main__coinDetails__heading">
+        <h1>
+          {cryptoDetails.name} ({cryptoDetails.symbol}) Live Price in USD
+        </h1>
+        <p> {cryptoDetails.description}</p>
+      </div>
 
-        <Col className="other-stats-info">
-          <Col className="coin-value-statistics-heading">
-            <Title level={3} className="coin-details-heading">
-              Other statistics
-            </Title>
-            <p>An overview showing the stats of all cryptocurrencies</p>
-          </Col>
-          {genericStats.map(({ icon, title, value }) => (
-            <Col className="coin-stats">
-              <Col className="coin-stats-name">
-                <Text>{icon}</Text>
-                <Text>{title}</Text>
-              </Col>
-              <Text className="stats">{value}</Text>
-            </Col>
+      <div className="main__coinDetails__stats">
+        <div className="main__coinDetails__stats__card">
+          <h2>{cryptoDetails.name} Statistics</h2>
+          {stats.map(({ icon, title, value }) => (
+            <div className="main__coinDetails__stats__info">
+              <div>
+                <span>{icon}</span>
+                <span>{title}</span>
+              </div>
+              <p>{value}</p>
+            </div>
           ))}
-        </Col>
-      </Col>
-      <Col className="coin-desc-link">
-        <Row className="coin-desc">
-          <Title level={3} className="coin-details-heading">
-            What is {cryptoDetails.name}?
-          </Title>
-          {cryptoDetails.description}
-        </Row>
-        <Col className="coin-links">
-          <Title level={3} className="coin-details-heading">
-            {cryptoDetails.name} Links
-            {cryptoDetails.links.map((link) => (
-              <Row className="coin-link" key={link.name}>
-                <Title level={5} className="link-name">
-                  {link.type}
-                </Title>
-                <a href={link.url} target="_blank" rel="noreferrer">
-                  {link.name}
-                </a>
-              </Row>
-            ))}
-          </Title>
-        </Col>
-      </Col>
-    </Col>
+        </div>
+
+        <div className="main__coinDetails__stats__card">
+          <h2 className="coin-details-heading">Other statistics</h2>
+          {genericStats.map(({ icon, title, value }) => (
+            <div className="main__coinDetails__stats__info">
+              <div>
+                <span>{icon}</span>
+                <span>{title}</span>
+              </div>
+              <p>{value}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="main__coinDetails__stats__card">
+          <h2> {cryptoDetails.name} Links</h2>
+
+          {cryptoDetails.links.map((link) => (
+            <div className="main__coinDetails__stats__info" key={link.name}>
+              <h3>{link.type}</h3>
+              <a href={link.url} target="_blank" rel="noreferrer">
+                {link.name}
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
